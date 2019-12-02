f5897e286ea156ad8708a733e8b7cf9fee0ec5e2
Martin Page <wookoouk@gmail.com>
2019-12-02 16:48:26 +0000

    convert `sign` to promise

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -2,97 +2,100 @@ import jwt from "jsonwebtoken";
 import Group from "../models/Group";
 
 
-
 export function _toSafeName(unsafeName) {
-  return unsafeName
-    .replace("&", "and")
-    .replace(/[^a-z0-9]/gi, "_")
-    .toLowerCase();
+    return unsafeName
+        .replace("&", "and")
+        .replace(/[^a-z0-9]/gi, "_")
+        .toLowerCase();
 }
 
 export function generateSafeName(name, list) {
-  return new Promise((good, bad) => {
-    const safeName = this._toSafeName(name);
-    let canHave = false;
-    let testName = safeName;
-    let testCount = 1;
+    return new Promise((good, bad) => {
+        const safeName = this._toSafeName(name);
+        let canHave = false;
+        let testName = safeName;
+        let testCount = 1;
 
-    const filter = function(res) {
-      return res.safeName.toLowerCase() === testName.toLowerCase();
-    };
+        const filter = function (res) {
+            return res.safeName.toLowerCase() === testName.toLowerCase();
+        };
 
-    while (!canHave) {
-      const dupes = list.filter(filter);
+        while (!canHave) {
+            const dupes = list.filter(filter);
 
-      if (dupes.length) {
-        testCount += 1;
-        testName = safeName + "_" + testCount;
-      } else {
-        canHave = true;
-        good(testName);
-      }
-    }
-  });
+            if (dupes.length) {
+                testCount += 1;
+                testName = safeName + "_" + testCount;
+            } else {
+                canHave = true;
+                good(testName);
+            }
+        }
+    });
 }
 
 export function getUserFromRequest(req) {
-  return new Promise((good, bad) => {
-    const authorizationHeader = req.headers.authorization;
-    if (
-      authorizationHeader &&
-      authorizationHeader.split(" ")[0] &&
-      authorizationHeader.split(" ")[0].toLowerCase() === "bearer"
-    ) {
-      try {
-        const decoded = jwt.verify(
-          authorizationHeader.split(" ")[1],
-          process.env.JWT_SECRET
-        );
-        good(decoded);
-      } catch (err) {
-        bad(err);
-      }
-    } else {
-      good();
-    }
-  });
+    return new Promise((good, bad) => {
+        const authorizationHeader = req.headers.authorization;
+        if (
+            authorizationHeader &&
+            authorizationHeader.split(" ")[0] &&
+            authorizationHeader.split(" ")[0].toLowerCase() === "bearer"
+        ) {
+            try {
+                const decoded = jwt.verify(
+                    authorizationHeader.split(" ")[1],
+                    process.env.JWT_SECRET
+                );
+                good(decoded);
+            } catch (err) {
+                bad(err);
+            }
+        } else {
+            good();
+        }
+    });
 }
 
 export function getUserForToken(user) {
-  return new Promise((good, bad) => {
-    let fullName = user.displayName;
-    if (user.givenName && user.sn) {
-      fullName = user.givenName + " " + user.sn;
-    }
+    return new Promise((good, bad) => {
+        let fullName = user.displayName;
+        if (user.givenName && user.sn) {
+            fullName = user.givenName + " " + user.sn;
+        }
 
-    let email = user.mail;
-    if (email) {
-      email = email.toLowerCase();
-    }
+        let email = user.mail;
+        if (email) {
+            email = email.toLowerCase();
+        }
 
-    const isAdmin = user.username === "admin";
+        const isAdmin = user.username === "admin";
 
-    // const groupsIDS = [];
+        // const groupsIDS = [];
 
-    Group.GroupsIAmIn({ memberOf: user.memberOf, isAdmin })
-      .then(groups => {
-        const groupIDS = groups.map(g => g.id);
+        Group.GroupsIAmIn({memberOf: user.memberOf, isAdmin})
+            .then(groups => {
+                const groupIDS = groups.map(g => g.id);
 
-        return good({
-          username: user.username || user.uid,
-          name: user.fullName || fullName,
-          company: user.company,
-          email: user.email || email,
-          groups: groupIDS
-          // icon: User.GetIcon(user.username)
-        });
-      })
-      .catch(err => {
-        return bad(err);
-      });
-  });
+                return good({
+                    username: user.username || user.uid,
+                    name: user.fullName || fullName,
+                    company: user.company,
+                    email: user.email || email,
+                    groups: groupIDS
+                    // icon: User.GetIcon(user.username)
+                });
+            })
+            .catch(err => {
+                return bad(err);
+            });
+    });
 }
+
 export function sign(user) {
-  return jwt.sign(user, process.env.JWT_SECRET);
+    return new Promise((good, bad) => {
+        const token = jwt.sign(user, process.env.JWT_SECRET);
+        good(token);
+    })
 }
 
