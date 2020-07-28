7b715bac1ecae0f2ed84823db32b11aa67c41972
George Deeks <george@neontribe.co.uk>
2020-07-28 10:00:46 +0100

    ensure local db connection

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose')
 const PORT = process.env.PORT;
 
 try {
+    // will create database if it can't see one
+
+    // martin doesn't expose mongodb outside of the web server; you'd never want to make that available
+    // otherwise people could brute force attack
+    // even if you tried to do this, the server would probably complain
+    // you probably dont want to tinker the live db, just the local db (e.g. modify a model file)
     mongoose.connect('mongodb://localhost:27017/komondor', { useNewUrlParser: true });
 } catch (err) {
     console.error(err);
