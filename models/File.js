d85f07093f85046f145f235583dcd09fa4f75634
George Deeks <george@neontribe.co.uk>
2020-08-05 13:24:40 +0100

    refactor: auto make function async

--- models/File.js
+++ models/File.js
@@ -12,23 +12,22 @@ const schema = new mongoose.Schema({
     tempUploadPath: { type: String, required: true },
 }, { timestamps: true, toJSON: { virtuals: true } });
 
-
-schema.methods.moveToFolderAndSave = function (relNewPath) {
+// i converted to async function, check this still works
+schema.methods.moveToFolderAndSave = async function (relNewPath) {
     const file = this;
     const fullNewPath = path.join(process.env.DATASTORE_ROOT, relNewPath);
 
-    // console.log('HELLO! moving', file.path, 'to', fullNewPath)
 
-    return fs.promises.rename(file.path, fullNewPath)
-        .then(() => {
-            // console.log('NEED to update path property for File')
-            // console.log('old path', file.path, 'new path', relNewPath)
-            file.path = fullNewPath; // martin had it as relNewPath
-            return file.save()
-        })
-        .catch(err => {
-            console.error(err);
-        })
+    try {
+        await fs.promises.rename(file.path, fullNewPath);
+        // console.log('NEED to update path property for File')
+        // console.log('old path', file.path, 'new path', relNewPath)
+        file.path = fullNewPath; // martin had it as relNewPath
+        return file.save();
+    }
+    catch (err) {
+        console.error(err);
+    }
 }
 
 const File = mongoose.model('File', schema);
