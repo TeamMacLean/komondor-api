d3eb20df9f38714e7a2ac1587a7058df9fa2d040
George Deeks <george@neontribe.co.uk>
2020-11-27 12:35:00 +0000

    fix: use yarn in test route

--- routes/test.js
+++ routes/test.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 router.route('/test')
     .get((req, res) => {
-        res.status(200).send({ message: "Hello there, good sir!" })
+        res.status(200).send({ message: "Hello there, good sir! Use yarn!" })
     });
 
 module.exports = router;
