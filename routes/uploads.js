1258b5bc7c18b9e43fb757de169dd8c51849c413
George Deeks <george@neontribe.co.uk>
2020-11-30 20:19:19 +0000

    fix: ensure HEAD req method

--- routes/uploads.js
+++ routes/uploads.js
@@ -14,30 +14,28 @@ tusServer.datastore = new tus.FileStore({
   path: "/uploads" //'/uploads'
 });
 
-tusServer.on('*', event => {
-  console.log('EVENT!', event)
-})
+// tusServer.on('*', event => {
+//   console.log('EVENT!', event)
+// })
 tusServer.on(tus.EVENTS.EVENT_UPLOAD_COMPLETE, event => {
-  console.log(tus.EVENTS.EVENT_UPLOAD_COMPLETE)
+  // Fired when a PATCH request finishes writing the file
+  console.log('EVENT_UPLOAD_COMPLETE', event)
   //fileUpload.create(event)
 });
 
 tusServer.on(tus.EVENTS.EVENT_FILE_CREATED, event => {
-  console.log(tus.EVENTS.EVENT_FILE_CREATED)
+  console.log('EVENT_FILE_CREATED', event)
 });
 tusServer.on(tus.EVENTS.EVENT_ENDPOINT_CREATED, event => {
-  console.log(tus.EVENTS.EVENT_ENDPOINT_CREATED)
+  console.log('EVENT_ENDPOINT_CREATED', event)
 });
 
 // const express = require('express');
 const uploadApp = express();
 uploadApp.all("*", function (req, res, next) {
 
-  // George freestyling
+  // This doesnt work
   // req.setHeader('Access-Control-Allow-Origin', '*');
-  // req.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  // req.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, Origin, Accept');
-  // req.setHeader('Access-Control-Allow-Credentials', false);
 
   console.log('useful request info: ' + 
     '\nreq.method', (req && req.method) ? req.method : 'unknown',
@@ -47,15 +45,12 @@ uploadApp.all("*", function (req, res, next) {
     '\nreq.getHeader(Access-Control-Allow-Methods)', req.get('Access-Control-Allow-Methods'),
     '\nreq.getHeader(Access-Control-Allow-Headers)', req.get('Access-Control-Allow-Headers'),
     '\nreq.getHeader(Access-Control-Allow-Credentials)', req.get('Access-Control-Allow-Credentials'),
-
-  );
-  
-  
+  );  
   
   // Website you wish to allow to connect
   res.setHeader('Access-Control-Allow-Origin', '*');
   // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, Authorization, Origin, Accept');
   // Set to true if you need the website to include cookies in the requests sent to the API (e.g. in case you use sessions)
@@ -66,6 +61,10 @@ uploadApp.all("*", function (req, res, next) {
   tusServer.handle.bind(tusServer)(req, res, next);
 })
 // uploadApp.all("*", tusServer.handle.bind(tusServer));
+
+// maybe one day, George
+// uploadApp.use('uploads, uploadApp')
+// uploadApp.listen(process.env.PORT, process.env.HOST)
 
 router.use("/uploads", uploadApp);
 
