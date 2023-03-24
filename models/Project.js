cd2bfb314832156d3d5131b5245abf7b53a25c02
George Deeks <georgedeeks@gmail.com>
2023-03-24 14:04:21 +0000

    refactor: prettier touched files

--- models/Project.js
+++ models/Project.js
@@ -1,171 +1,187 @@
-const mongoose = require('mongoose')
-const { join } = require('path')
-const generateSafeName = require('../lib/utils/generateSafeName').default
-const fs = require('fs')
-const NewsItem = require('./NewsItem')
+const mongoose = require("mongoose");
+const { join } = require("path");
+const generateSafeName = require("../lib/utils/generateSafeName").default;
+const fs = require("fs");
+const NewsItem = require("./NewsItem");
 
-const schema = new mongoose.Schema({
+const schema = new mongoose.Schema(
+  {
     name: { type: String, required: true, unique: true }, // keep unique but update UI to reflect this // TODO
     safeName: { type: String, required: true, unique: true },
     owner: { type: String, required: true },
     shortDesc: { type: String, required: true },
     longDesc: { type: String, required: true },
-    group: { type: mongoose.Schema.Types.ObjectId, ref: 'Group', required: true },
+    group: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Group",
+      required: true,
+    },
     isPublic: { type: Boolean, default: false },
 
     // GG new fields
-    oldId: {type: String}, // TODO i dont think i need this but hard to extract // warning, post migration i removed unique=true
-    oldSafeName: {type: String, unique: false, required: false}, // temp?
-    secondaryOwner: {type: String, required: false},
-    path: {type: String, required: false, unique: true}, // George add unique: true; surely required is true also? 
+    oldId: { type: String }, // TODO i dont think i need this but hard to extract // warning, post migration i removed unique=true
+    oldSafeName: { type: String, unique: false, required: false }, // temp?
+    secondaryOwner: { type: String, required: false },
+    path: { type: String, required: false, unique: true }, // George add unique: true; surely required is true also?
 
-    accessions: [{type: String, unique: false}], // unique except null TODO
-    releaseDate: {type: String, unique: false}, // 'DD-MM-YYYY' format; ENA
+    accessions: [{ type: String, unique: false }], // unique except null TODO
+    releaseDate: { type: String, unique: false }, // 'DD-MM-YYYY' format; ENA
 
     // TODO ensure each is unique?
     additionalFilesUploadIDs: [{ type: String }], // George has created (was missing with Martin)
 
+    // George, these 2 are entirely redundant
     doNotSendToEna: { type: Boolean, default: false },
-    doNotSendToEnaReason: { type: String }
-}, { timestamps: true, toJSON: { virtuals: true } });
+    doNotSendToEnaReason: { type: String },
 
-schema.pre('validate', async function () {
-    const allOthers = await Project.find({});
-    const safeName = await generateSafeName(this.name, allOthers.filter(f => f._id.toString() !== this._id.toString()));
-    this.safeName = safeName;
-    const doc = this;
-    const populatedDoc = await doc.populate({
-        path: 'group',
+    // NUDGE feature 13-9-2022
+    lastNudged: { type: Date, default: null },
+    doNotNudge: { type: Boolean, default: false },
+    nudgeable: { type: Boolean, default: true }, // all new Projects are nudgeable by default
+  },
+  { timestamps: true, toJSON: { virtuals: true } }
+);
+
+schema.pre("validate", async function () {
+  const allOthers = await Project.find({});
+  const safeName = await generateSafeName(
+    this.name,
+    allOthers.filter((f) => f._id.toString() !== this._id.toString())
+  );
+  this.safeName = safeName;
+  const doc = this;
+  const populatedDoc = await doc
+    .populate({
+      path: "group",
     })
-        .execPopulate();
-    try {
-        this.path = join('/', populatedDoc.group.safeName, populatedDoc.safeName);
-        return Promise.resolve();
-    } catch (e) {
-        return Promise.reject(e);
-    }
+    .execPopulate();
+  try {
+    this.path = join("/", populatedDoc.group.safeName, populatedDoc.safeName);
+    return Promise.resolve();
+  } catch (e) {
+    return Promise.reject(e);
+  }
 });
 
-schema.pre('save', function (next) {
-    this.wasNew = this.isNew;
-    next()
+schema.pre("save", function (next) {
+  this.wasNew = this.isNew;
+  next();
 });
 
-schema.post('save', async function (next) {
-    const doc = this;
-    
-    const alreadyMadeArray = await NewsItem.find(
-        {'typeId': doc._id}
+schema.post("save", async function (next) {
+  const doc = this;
+
+  const alreadyMadeArray = await NewsItem.find({ typeId: doc._id });
+  const alreadyMade = !!alreadyMadeArray.length;
+
+  if (alreadyMade) {
+    console.log(
+      "already made this project, so wont check dir or make newsitem"
     );
-    const alreadyMade = !!(alreadyMadeArray.length)
+    return Promise.resolve();
+  } else {
+    console.log("new project");
 
-    if (alreadyMade){
-        console.log('already made this project, so wont check dir or make newsitem')
+    async function createNewsItem() {
+      try {
+        const savedNewsItem = await new NewsItem({
+          type: "project",
+          typeId: doc._id,
+          owner: doc.owner,
+          group: doc.group._id || doc.group,
+          name: doc.name,
+          body: doc.shortDesc,
+        }).save();
         return Promise.resolve();
-    } else {
-        console.log('new project')
+      } catch (err) {
+        console.error(err);
+        return Promise.resolve();
+      }
+    }
 
-        async function createNewsItem() {
-            try {
-                const savedNewsItem = await new NewsItem({
-                    type: 'project',
-                    typeId: doc._id,
-                    owner: doc.owner,
-                    group: doc.group._id || doc.group,
-                    name: doc.name,
-                    body: doc.shortDesc
-                })
-                    .save();
-                return Promise.resolve();
-            } catch (err) {
-                console.error(err);
-                return Promise.resolve();
-            }
-        }
+    // PLEASE ADJUST OTHER MODELS
 
-        // PLEASE ADJUST OTHER MODELS
-
-        // create directory
-        const absPath = join(process.env.DATASTORE_ROOT, this.path);
-        try {
-            console.log('will make this dir', absPath, '...:');
-            await fs.promises.mkdir(absPath, {recursive: true});
-        } catch (e) {
-            console.log('...error mkdir of new project', e, absPath);
-            // find another way to mkdir
-            return Promise.reject(e)
-        }             
-        console.log('...dir creation (probably) successful, now create newsitem');
-        return createNewsItem();   
-    }     
+    // create directory
+    const absPath = join(process.env.DATASTORE_ROOT, this.path);
+    try {
+      console.log("will make this dir", absPath, "...:");
+      await fs.promises.mkdir(absPath, { recursive: true });
+    } catch (e) {
+      console.log("...error mkdir of new project", e, absPath);
+      // find another way to mkdir
+      return Promise.reject(e);
+    }
+    console.log("...dir creation (probably) successful, now create newsitem");
+    return createNewsItem();
+  }
 });
 
-schema.pre('bulkWrite', (next) => {
-    console.log('rooney')
-    next()
+schema.pre("bulkWrite", (next) => {
+  console.log("rooney");
+  next();
 });
-schema.pre('updateOne', (next) => {
-    const data = this.getUpdate();
-    console.log('reached pre!', data)
-    next()    
+schema.pre("updateOne", (next) => {
+  const data = this.getUpdate();
+  console.log("reached pre!", data);
+  next();
 });
-schema.post('update', (next) => {
-    const data = this.getUpdate();
-    console.log('reached post!', data)
-    next() 
-});
-
-schema.virtual('samples', {
-    ref: 'Sample',
-    localField: '_id',
-    foreignField: 'project',
-    justOne: false, // set true for one-to-one relationship
+schema.post("update", (next) => {
+  const data = this.getUpdate();
+  console.log("reached post!", data);
+  next();
 });
 
-schema.virtual('additionalFiles', {
-    ref: 'AdditionalFile',
-    localField: '_id',
-    foreignField: 'project',
-    justOne: false, // set true for one-to-one relationship
+schema.virtual("samples", {
+  ref: "Sample",
+  localField: "_id",
+  foreignField: "project",
+  justOne: false, // set true for one-to-one relationship
+});
+
+schema.virtual("additionalFiles", {
+  ref: "AdditionalFile",
+  localField: "_id",
+  foreignField: "project",
+  justOne: false, // set true for one-to-one relationship
 });
 
 schema.methods.getRelativePath = function () {
-    const doc = this;
-    return doc.populate({
-        path: 'group',
+  const doc = this;
+  return doc
+    .populate({
+      path: "group",
     })
-        .execPopulate()
-        .then(populatedDoc => {
-            return join(populatedDoc.group.safeName, populatedDoc.safeName)
-        })
-}
+    .execPopulate()
+    .then((populatedDoc) => {
+      return join(populatedDoc.group.safeName, populatedDoc.safeName);
+    });
+};
 
 schema.methods.getAbsPath = function getPath() {
-    const doc = this;
+  const doc = this;
 
-    return doc.getRelativePath()
-        .then(relPath => {
-            return join(process.env.DATASTORE_ROOT, relPath);
-        })
+  return doc.getRelativePath().then((relPath) => {
+    return join(process.env.DATASTORE_ROOT, relPath);
+  });
 };
 
 schema.statics.iCanSee = function iCanSee(user) {
-    
-    if (user.username === 'admin' || process.env.FULL_RECORDS_ACCESS_USERS.includes(user.username)) {
-        return Project.find({})
-    }
-    const filters = [
-        { 'owner': user.username }
-    ];
-    if (user.groups) {
-        user.groups.map(g => {
-            filters.push({ 'group': g })
-        });
-    }
-    
-    return Project.find({ $or: filters });
+  if (
+    user.username === "admin" ||
+    process.env.FULL_RECORDS_ACCESS_USERS.includes(user.username)
+  ) {
+    return Project.find({});
+  }
+  const filters = [{ owner: user.username }];
+  if (user.groups) {
+    user.groups.map((g) => {
+      filters.push({ group: g });
+    });
+  }
+
+  return Project.find({ $or: filters });
 };
 
-const Project = mongoose.model('Project', schema);
+const Project = mongoose.model("Project", schema);
 
 module.exports = Project;
--- routes/projects.js
+++ routes/projects.js
@@ -1,157 +1,172 @@
-const express = require("express")
+const express = require("express");
 let router = express.Router();
-const Project = require('../models/Project')
-const { isAuthenticated } = require('./middleware')
-const _path = require('path')
-const fs = require('fs');
-const { sortAdditionalFiles } = require('../lib/sortAssociatedFiles');
-const sendOverseerEmail = require('../lib/utils/sendOverseerEmail')
+const Project = require("../models/Project");
+const { isAuthenticated } = require("./middleware");
+const _path = require("path");
+const fs = require("fs");
+const { sortAdditionalFiles } = require("../lib/sortAssociatedFiles");
+const sendOverseerEmail = require("../lib/utils/sendOverseerEmail");
 
-router.route('/projects')
-    .all(isAuthenticated)
-    .get(async (req, res) => {
-        //TODO must be in same group as user
+router
+  .route("/projects")
+  .all(isAuthenticated)
+  .get(async (req, res) => {
+    //TODO must be in same group as user
 
-        try {
-            const limit = await Project.iCanSee(req.user)
-            // consider refactoring to reduce load
-            const orderByMostRecent = limit.sort((a, b) => {
-                const aDate = new Date(a.createdAt).getTime()
-                const bDate = new Date(b.createdAt).getTime()
-                
-                const res = (aDate > bDate) ? -1 : 1
-                //console.log('aDate', aDate, 'bDate', bDate, 'res', res);
-                return res
-            })
-            res.status(200).send({ projects: orderByMostRecent })
-        } catch (err) {
-            console.error('error!!!!!', err);
-            res.status(500).send({ error: err })
-        }
-    });
+    try {
+      const limit = await Project.iCanSee(req.user);
+      // consider refactoring to reduce load
+      const orderByMostRecent = limit.sort((a, b) => {
+        const aDate = new Date(a.createdAt).getTime();
+        const bDate = new Date(b.createdAt).getTime();
 
-router.route('/projects/names')
-    //.all(isAuthenticated)
-    .get(async (req, res) => {
-        Project.find({}).select('name')
-            .then((resPros => {
-                const results = resPros.map(resPro => resPro.name)
-                res.status(200).send({ projectsNames: results })
-            }))
-            .catch(err => {
-                console.error('error!!!!!', err);
-                res.status(500).send({ error: err })
-            });        
-});
+        const res = aDate > bDate ? -1 : 1;
+        //console.log('aDate', aDate, 'bDate', bDate, 'res', res);
+        return res;
+      });
+      res.status(200).send({ projects: orderByMostRecent });
+    } catch (err) {
+      console.error("error!!!!!", err);
+      res.status(500).send({ error: err });
+    }
+  });
 
-router.route('/project')
-    .all(isAuthenticated)
-    .get((req, res) => {
+router
+  .route("/projects/names")
+  //.all(isAuthenticated)
+  .get(async (req, res) => {
+    Project.find({})
+      .select("name")
+      .then((resPros) => {
+        const results = resPros.map((resPro) => resPro.name);
+        res.status(200).send({ projectsNames: results });
+      })
+      .catch((err) => {
+        console.error("error!!!!!", err);
+        res.status(500).send({ error: err });
+      });
+  });
 
-        if (!req.query.id) {
-            res.status(500).send({ error: new Error('param :id not provided') })
-        } else {
+router
+  .route("/project")
+  .all(isAuthenticated)
+  .get((req, res) => {
+    if (!req.query.id) {
+      res.status(500).send({ error: new Error("param :id not provided") });
+    } else {
+      // proceed
+
+      Project.findById(req.query.id)
+        .populate("group")
+        .populate({ path: "samples", populate: { path: "group" } })
+        .populate({ path: "additionalFiles", populate: { path: "file" } })
+        .then((project) => {
+          if (!project) {
+            res.status(501).send({ error: "Project not found" });
+          } else {
             // proceed
 
-            Project
-                .findById(req.query.id)
-                .populate('group')
-                .populate({ path: 'samples', populate: { path: 'group' } })
-                .populate({ path: 'additionalFiles', populate: { path: 'file' } })
-                .then(project => {
-                    if (!project) {
-                        res.status(501).send({ error: 'Project not found' });
-                    } else {                       
-                        // proceed
-                        
-                        try {                         
-                            
-                            const dirRoot = _path.join(process.env.DATASTORE_ROOT, project.path);
-                            const additionalDir = _path.join(dirRoot, 'additional');
+            try {
+              const dirRoot = _path.join(
+                process.env.DATASTORE_ROOT,
+                project.path
+              );
+              const additionalDir = _path.join(dirRoot, "additional");
 
-                            fs.stat(additionalDir, function(err, stats) {
-
-                                // handle errors as dir probably nonexistent
-                                if (err || !stats.isDirectory()){                                    
-                                    res.status(200).send({
-                                        project: project,
-                                        actualAdditionalFiles: [],
-                                    });
-                                } else {
-                                    fs.readdir(additionalDir, (additionalFilesErr, additionalFiles) => {
-                                        // throw errors as unexpected
-                                        if (additionalFilesErr){
-                                            throw new Error(additionalFilesErr)
-                                        }
-                                        res.status(200).send({
-                                            project: project,
-                                            actualAdditionalFiles: additionalFiles,
-                                        });
-                                    })  
-                                }                                
-                            });
-                        } catch (e) {
-                            console.error(e, e.message)
-                            res.status(501).send({error: 'Unexpected readdir error'})
-                        }
-                    }
-                })
-                .catch(err => {
-                    // error during db inflation
-                    console.error(e, e.message)
-                    res.status(500).send({ error: err });
-                })
-            ;
-        }
-    }
-);
-
-router.route('/projects/new')
-    .all(isAuthenticated)
-    .post((req, res) => {
-        //TODO check permission
-
-        const newProject = new Project({
-            name: req.body.name,
-            group: req.body.group,
-            shortDesc: req.body.shortDesc,
-            longDesc: req.body.longDesc,
-            owner: req.body.owner,
-            additionalFilesUploadID: req.body.additionalUploadID,
-            doNotSendToEna: req.body.doNotSendToEna,
-            doNotSendToEnaReason: req.body.doNotSendToEnaReason,
-            oldId: Math.random().toString(16).substr(2, 6), // TODO remove
-        });
-
-        let setAsSavedProject;
-        newProject.save()
-            .then(async savedProject => {
-                setAsSavedProject = savedProject;
-                const additionalFiles = req.body.additionalFiles;
-
-                if (additionalFiles.length){
-                    try {
-                        return await sortAdditionalFiles(additionalFiles, 'project', setAsSavedProject._id, setAsSavedProject.path)
-                    } catch (e){
-                        // if issue with files, remove newProject
-                        await Project.deleteOne({ '_id': setAsSavedProject._id });  
-                        
-                        // TODO great chance to report back to user what is saved and what is not!!!
-
-                        return Promise.reject(e);
-                    }
+              fs.stat(additionalDir, function (err, stats) {
+                // handle errors as dir probably nonexistent
+                if (err || !stats.isDirectory()) {
+                  res.status(200).send({
+                    project: project,
+                    actualAdditionalFiles: [],
+                  });
                 } else {
-                    return Promise.resolve()
+                  fs.readdir(
+                    additionalDir,
+                    (additionalFilesErr, additionalFiles) => {
+                      // throw errors as unexpected
+                      if (additionalFilesErr) {
+                        throw new Error(additionalFilesErr);
+                      }
+                      res.status(200).send({
+                        project: project,
+                        actualAdditionalFiles: additionalFiles,
+                      });
+                    }
+                  );
                 }
-            })
-            .then(() => { 
-                sendOverseerEmail({type: 'Project', data: setAsSavedProject}).then((emailResult) => {
-                    res.status(200).send({ project: setAsSavedProject })
-                })
-            })
-            .catch(err => {
-                res.status(500).send({ error: err })
-            })
+              });
+            } catch (e) {
+              console.error(e, e.message);
+              res.status(501).send({ error: "Unexpected readdir error" });
+            }
+          }
+        })
+        .catch((err) => {
+          // error during db inflation
+          console.error(e, e.message);
+          res.status(500).send({ error: err });
+        });
+    }
+  });
+
+router
+  .route("/projects/new")
+  .all(isAuthenticated)
+  .post((req, res) => {
+    //TODO check permission
+
+    const newProject = new Project({
+      name: req.body.name,
+      group: req.body.group,
+      shortDesc: req.body.shortDesc,
+      longDesc: req.body.longDesc,
+      owner: req.body.owner,
+      additionalFilesUploadID: req.body.additionalUploadID,
+      doNotSendToEna: req.body.doNotSendToEna,
+      doNotSendToEnaReason: req.body.doNotSendToEnaReason,
+      oldId: Math.random().toString(16).substr(2, 6), // TODO remove
+      doNotNudge: false,
+      nudgeable: true,
     });
+
+    let setAsSavedProject;
+    newProject
+      .save()
+      .then(async (savedProject) => {
+        setAsSavedProject = savedProject;
+        const additionalFiles = req.body.additionalFiles;
+
+        if (additionalFiles.length) {
+          try {
+            return await sortAdditionalFiles(
+              additionalFiles,
+              "project",
+              setAsSavedProject._id,
+              setAsSavedProject.path
+            );
+          } catch (e) {
+            // if issue with files, remove newProject
+            await Project.deleteOne({ _id: setAsSavedProject._id });
+
+            // TODO great chance to report back to user what is saved and what is not!!!
+
+            return Promise.reject(e);
+          }
+        } else {
+          return Promise.resolve();
+        }
+      })
+      .then(() => {
+        sendOverseerEmail({ type: "Project", data: setAsSavedProject }).then(
+          (emailResult) => {
+            res.status(200).send({ project: setAsSavedProject });
+          }
+        );
+      })
+      .catch((err) => {
+        res.status(500).send({ error: err });
+      });
+  });
 
 module.exports = router;
