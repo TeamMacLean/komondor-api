34a589c63160c333dfd672185e4cce3c5f756e1c
George Deeks <georgedeeks@gmail.com>
2024-04-10 12:27:54 +0100

    feat: local one off queries

---
+++ one-off.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+const Sample = require("./models/Sample"); // Update the path as necessary
+
+mongoose
+  .connect("mongodb://localhost:27017/komondor", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB");
+
+    // Your query
+    Sample.findOne({}, "_id safeName", { sort: { createdAt: -1 } }) // Sorting by createdAt in descending order
+      .then((result) => {
+        console.log(result);
+        mongoose.disconnect(); // Disconnect after the operation is complete
+      })
+      .catch((err) => {
+        console.error(err);
+        mongoose.disconnect();
+      });
+  })
+  .catch((err) => {
+    console.error("Connection error", err);
+  });
