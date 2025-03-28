68f19f41fc5774a54bc2e862734255d79bad2348
George Deeks <georgedeeks@gmail.com>
2025-03-28 14:31:54 +0000

    fix: typo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
-const moongoosePort = process.env.MONGODB_PORT || 27017;
+const mongoosePort = process.env.MONGODB_PORT || 27017;
 
 try {
   // will create database if it can't see one
@@ -11,18 +11,21 @@ try {
   // even if you tried to do this, the server would probably complain
   // you probably dont want to tinker the live db, just the local db (e.g. modify a model file)
 
-  mongoose.connect(`mongodb://localhost:${mongoosePort}/komondor`, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useUnifiedTopology: true,
-    serverSelectionTimeoutMS: 10000 // 10 seconds
-  }).then(() => {
-    console.log('Connected to MongoDB');
-  }).catch(err => {
-    console.error('Error connecting to MongoDB', err);
-  });
+  mongoose
+    .connect(`mongodb://localhost:${mongoosePort}/komondor`, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // 10 seconds
+    })
+    .then(() => {
+      console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+      console.error("Error connecting to MongoDB", err);
+    });
 } catch (err) {
-  console.error('Unexpected error:', err);
+  console.error("Unexpected error:", err);
 }
 
 app.listen(PORT, () => console.log(`API running on port ${PORT}!`));
