ca233a6098afbc6238b1078e6e488e229cd9d7b5
George Deeks <georgedeeks@gmail.com>
2025-06-11 14:46:23 +0100

    feat: tplex sample

--- models/Sample.js
+++ models/Sample.js
@@ -1,123 +1,132 @@
 //import { Schema, model } from 'mongoose';
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 const { Schema, model } = mongoose;
 //import { join } from 'path';
-const NewsItem = require('./NewsItem')
+const NewsItem = require("./NewsItem");
 
-const generateSafeName = require('../lib/utils/generateSafeName').default
-const fs = require('fs')
-const { join } = require('path');
+const generateSafeName = require("../lib/utils/generateSafeName").default;
+const fs = require("fs");
+const { join } = require("path");
 
-const schema = new Schema({
-  name: { type: String, required: true }, // should NOT have unique, rely on path instead
-  safeName: { type: String, required: true }, //should have unique
-  project: { type: Schema.Types.ObjectId, ref: 'Project', required: true },
-  scientificName: { type: String, required: true },
-  commonName: { type: String, required: true },
-  ncbi: { type: String, required: true },
-  conditions: { type: String, required: true },
-  owner: { type: String, required: true },
-  group: { type: Schema.Types.ObjectId, ref: 'Group', required: true },
+const schema = new Schema(
+  {
+    name: { type: String, required: true }, // should NOT have unique, rely on path instead
+    safeName: { type: String, required: true }, //should have unique
+    project: { type: Schema.Types.ObjectId, ref: "Project", required: true },
+    scientificName: { type: String, required: true },
+    commonName: { type: String, required: true },
+    ncbi: { type: String, required: true },
+    conditions: { type: String, required: true },
+    owner: { type: String, required: true },
+    group: { type: Schema.Types.ObjectId, ref: "Group", required: true },
 
-  oldId: {type: String},
+    oldId: { type: String },
 
-  accessions: [{type: String, unique: false}], // unique except null TODO
+    accessions: [{ type: String, unique: false }], // unique except null TODO
 
-  // TODO ensure each is unique?
-  additionalFilesUploadIDs: [{ type: String }], // George has changed to array and renamed
+    // TODO ensure each is unique?
+    additionalFilesUploadIDs: [{ type: String }], // George has changed to array and renamed
 
-  // GG added
-  path: {type: String, required: false, unique: true}, // George add unique: true; surely required is true also? 
-  oldSafeName: {type: String, unique: false, required: false}, // temp?
-  sampleGroup: {type: String, required: false},
-  // i could add oldProjectID, but I dont see the point
+    // GG added
+    path: { type: String, required: false, unique: true }, // George add unique: true; surely required is true also?
+    oldSafeName: { type: String, unique: false, required: false }, // temp?
+    sampleGroup: { type: String, required: false },
+    // i could add oldProjectID, but I dont see the point
 
-  // not sure if 'required' cos its in validate function, TODO check
-  //originallyAdded: {type: Number} // timestamp (w. 2dp) from original datahog, OR timestamp from creation (i.e. komondor)
-}, { timestamps: true, toJSON: { virtuals: true } });
+    // tplex
+    tplexCsv: { type: String, required: false }, // Only store the raw CSV string
 
-schema.pre('validate', function () {
+    // not sure if 'required' cos its in validate function, TODO check
+    //originallyAdded: {type: Number} // timestamp (w. 2dp) from original datahog, OR timestamp from creation (i.e. komondor)
+  },
+  { timestamps: true, toJSON: { virtuals: true } },
+);
+
+schema.pre("validate", function () {
   return Sample.find({})
-    .then(allOthers => {
-      // if (this.safeName){
-      //   console.log('already has a safename, so not generating one');
-      // }
-      return this.safeName || generateSafeName(this.name, allOthers.filter(f => f._id.toString() !== this._id.toString()));
+    .then((allOthers) => {
+      return (
+        this.safeName ||
+        generateSafeName(
+          this.name,
+          allOthers.filter((f) => f._id.toString() !== this._id.toString()),
+        )
+      );
     })
-    .then(safeName => {
+    .then((safeName) => {
       this.safeName = safeName;
-      // if (!this.originallyAdded){
-      //   this.originallyAdded = Date.now();
-      // }
       const doc = this;
-      return doc.populate({
-          path: 'project',
-      })
-          .execPopulate()
-          .then(populatedDoc => {
-            //console.log('poplateddoc', populatedDoc);
-            
-              try {                        
-                  this.path = join(populatedDoc.project.path, populatedDoc.safeName)                   
-                  return Promise.resolve()                                             
-              } catch (e) {
-                  return Promise.reject(e)
-              }
-          })
-    })
+      return doc
+        .populate({
+          path: "project",
+        })
+        .execPopulate()
+        .then((populatedDoc) => {
+          try {
+            this.path = join(populatedDoc.project.path, populatedDoc.safeName);
+            return Promise.resolve();
+          } catch (e) {
+            return Promise.reject(e);
+          }
+        });
+    });
 });
 
-schema.pre('save', function (next) {
+schema.pre("save", function (next) {
   this.wasNew = this.isNew;
-  next()
+
+  if (!this.tplexCsv) {
+    this.tplexCsv = undefined;
+  }
+
+  next();
 });
 
-schema.post('save', async function (next) {
+schema.post("save", async function (next) {
   const doc = this;
 
-  const alreadyMadeArray = await NewsItem.find(
-    {'typeId': doc._id}
-  );
-  const alreadyMade = !!(alreadyMadeArray.length)
+  const alreadyMadeArray = await NewsItem.find({ typeId: doc._id });
+  const alreadyMade = !!alreadyMadeArray.length;
 
   if (alreadyMade) {
-    console.log('Sample already a newsitem, so not creating that or making directory')
-    return Promise.resolve();
+    console.log(
+      "Sample already a newsitem, so not creating that or making directory",
+    );
   } else {
     //create news item
-    return new NewsItem({
-      type: 'sample',
+    await new NewsItem({
+      type: "sample",
       typeId: doc._id,
       owner: doc.owner,
       group: doc.group,
       name: doc.name,
       body: doc.conditions,
       //originallyAdded: doc.originallyAdded,
-    })
-      .save()
-      .then(() => {
-        // create directory
-        const absPath = join(process.env.DATASTORE_ROOT, this.path);     
-        return fs.promises.mkdir(absPath) 
-      })
-      .catch(err => {
-        console.error(err);
-        Promise.resolve();
-      })
-  }  
+    }).save();
+
+    // create directory
+    const absPath = join(process.env.DATASTORE_ROOT, this.path);
+    await fs.promises.mkdir(absPath, { recursive: true }).catch((err) => {
+      if (err.code === "EEXIST") {
+        console.log(`Directory ${absPath} already exists.`);
+      } else {
+        console.error("Error creating directory for sample:", err);
+      }
+    });
+  }
 });
 
-schema.virtual('runs', {
-  ref: 'Run',
-  localField: '_id',
-  foreignField: 'sample',
+schema.virtual("runs", {
+  ref: "Run",
+  localField: "_id",
+  foreignField: "sample",
   justOne: false, // set true for one-to-one relationship
 });
 
-schema.virtual('additionalFiles', {
-  ref: 'AdditionalFile',
-  localField: '_id',
-  foreignField: 'sample',
+schema.virtual("additionalFiles", {
+  ref: "AdditionalFile",
+  localField: "_id",
+  foreignField: "sample",
   justOne: false, // set true for one-to-one relationship
 });
 
@@ -125,50 +134,51 @@ schema.methods.getRelativePath = function () {
   const doc = this;
   return doc
     .populate({
-      path: 'group',
+      path: "group",
     })
     .populate({
-      path: 'project'
+      path: "project",
     })
     .execPopulate()
-    .then(populatedDoc => {
-      return join(populatedDoc.group.safeName, populatedDoc.project.safeName, populatedDoc.safeName)
-    })
-}
+    .then((populatedDoc) => {
+      return join(
+        populatedDoc.group.safeName,
+        populatedDoc.project.safeName,
+        populatedDoc.safeName,
+      );
+    });
+};
 
 schema.methods.getAbsPath = function getPath() {
   const doc = this;
 
-  return doc.getRelativePath()
-    .then(relPath => {
-      return join(process.env.DATASTORE_ROOT, relPath);
-    })
+  return doc.getRelativePath().then((relPath) => {
+    return join(process.env.DATASTORE_ROOT, relPath);
+  });
 };
 
 schema.statics.iCanSee = function iCanSee(user) {
-  if (user.username === 'admin' || process.env.FULL_RECORDS_ACCESS_USERS.includes(user.username)) {
-    return Sample.find({})
+  if (
+    user.username === "admin" ||
+    process.env.FULL_RECORDS_ACCESS_USERS.includes(user.username)
+  ) {
+    return Sample.find({});
   }
-  const filters = [
-    { 'owner': user.username }
-  ];
+  const filters = [{ owner: user.username }];
   if (user.groups) {
-    user.groups.map(g => {
-      filters.push({ 'group': g })
+    user.groups.map((g) => {
+      filters.push({ group: g });
     });
   }
-  return Sample.find({ $or: filters })
+  return Sample.find({ $or: filters });
 };
 
 schema.methods.toENA = function toENA() {
-
   const sample = this;
 
-  js2xmlparser.parse("sample", sample)
+  js2xmlparser.parse("sample", sample);
+};
 
-}
+const Sample = model("Sample", schema);
 
-
-const Sample = model('Sample', schema);
-
-module.exports = Sample
+module.exports = Sample;
--- routes/samples.js
+++ routes/samples.js
@@ -1,128 +1,140 @@
-const express = require("express")
+const express = require("express");
 let router = express.Router();
-const Sample = require('../models/Sample')
-const File = require('../models/File');
-const AdditionalFile = require('../models/AdditionalFile');
-const { isAuthenticated } = require('./middleware')
-const _path = require('path')
-const fs = require('fs');
-const { sortAdditionalFiles } = require('../lib/sortAssociatedFiles');
-const sendOverseerEmail = require('../lib/utils/sendOverseerEmail')
+const Sample = require("../models/Sample"); // Make sure this path is correct
+const File = require("../models/File"); // Make sure this path is correct
+const AdditionalFile = require("../models/AdditionalFile"); // Make sure this path is correct
+const { isAuthenticated } = require("./middleware"); // Make sure this path is correct
+const _path = require("path");
+const fs = require("fs");
+const { sortAdditionalFiles } = require("../lib/sortAssociatedFiles"); // Make sure this path is correct
+const sendOverseerEmail = require("../lib/utils/sendOverseerEmail"); // Make sure this path is correct
 
-router.route('/samples')
-    .all(isAuthenticated)
-    .get((req, res) => {
-        //TODO must be in same group as user
-        Sample.iCanSee(req.user)
-            .populate('group')
-            .sort('-createdAt')
-            .then(samples => {
-                res.status(200).send({ samples })
-            })
-            .catch(err => {
-                res.status(500).send({ error: err })
-            });
+router
+  .route("/samples")
+  .all(isAuthenticated)
+  .get((req, res) => {
+    //TODO must be in same group as user
+    Sample.iCanSee(req.user)
+      .populate("group")
+      .sort("-createdAt")
+      .then((samples) => {
+        res.status(200).send({ samples });
+      })
+      .catch((err) => {
+        res.status(500).send({ error: err });
+      });
+  });
 
-    });
+router
+  .route("/sample")
+  .all(isAuthenticated)
+  .get((req, res) => {
+    if (req.query.id) {
+      Sample.findById(req.query.id)
+        .populate("group")
+        .populate("project")
+        .populate({ path: "runs", populate: { path: "group" } })
+        .populate({ path: "additionalFiles", populate: { path: "file" } })
+        .then((sample) => {
+          //TODO check they have permissions
+          if (sample) {
+            try {
+              const dirRoot = _path.join(
+                process.env.DATASTORE_ROOT,
+                sample.path,
+              );
+              const additionalDir = _path.join(dirRoot, "additional");
 
-router.route('/sample')
-    .all(isAuthenticated)
-    .get((req, res) => {
-        if (req.query.id) {
-
-            Sample.findById(req.query.id)
-                .populate('group')
-                .populate('project')
-                .populate({ path: 'runs', populate: { path: 'group' } })
-                .populate({ path: 'additionalFiles', populate: { path: 'file' } })
-                .then(sample => {
-                    //TODO check they have permissions
-                    if (sample) {
-                        try {                            
-                            const dirRoot = _path.join(process.env.DATASTORE_ROOT, sample.path);
-                            const additionalDir = _path.join(dirRoot, 'additional');
-
-                            fs.stat(additionalDir, function(err, stats) {
-                                if (err || !stats.isDirectory()){                                    
-                                    res.status(200).send({
-                                        sample: sample,
-                                        actualAdditionalFiles: [],
-                                    });
-                                } else {
-                                    fs.readdir(additionalDir, (additionalFilesErr, additionalFiles) => {
-                                        if (additionalFilesErr){
-                                            throw new Error(additionalFilesErr)
-                                        }
-                                        res.status(200).send({
-                                            sample: sample,
-                                            actualAdditionalFiles: additionalFiles,
-                                        });
-                                    })  
-                                }                                
-                            });
-                        } catch (e) {
-                            console.error(e, e.message)
-                            res.status(501).send({error: 'unexpected readdir error'})
-                        }
-                    } else {
-                        res.status(501).send({ error: 'not found' });
-                    }
-                })
-                .catch(err => {
-                    res.status(500).send({ error: err });
-                })
-
-        } else {
-            res.status(500).send({ error: new Error('param :id not provided') })
-        }
-    });
-
-router.route('/samples/new')
-    .all(isAuthenticated)
-    .post((req, res) => {
-        const newSample = new Sample({
-            name: req.body.name,
-            project: req.body.project,
-            scientificName: req.body.scientificName,
-            commonName: req.body.commonName,
-            ncbi: req.body.ncbi,
-            conditions: req.body.conditions,
-            owner: req.body.owner,
-            group: req.body.group,
-        })
-
-        let returnedSample;
-        newSample.save()
-            .then(async savedSample => {
-                returnedSample = savedSample;
-                const additionalFiles = req.body.additionalFiles;
-
-                if (additionalFiles.length){
-                    try {
-                        return await sortAdditionalFiles(additionalFiles, 'sample', returnedSample._id, returnedSample.path)
-                    } catch (e){
-                        // if issue with files, remove newProject
-                        await Sample.deleteOne({ '_id': returnedSample._id });                        
-                        return Promise.reject(e);
-                    }
+              fs.stat(additionalDir, function (err, stats) {
+                if (err || !stats.isDirectory()) {
+                  res.status(200).send({
+                    sample: sample,
+                    actualAdditionalFiles: [],
+                  });
                 } else {
-                    return Promise.resolve()
+                  fs.readdir(
+                    additionalDir,
+                    (additionalFilesErr, additionalFiles) => {
+                      if (additionalFilesErr) {
+                        throw new Error(additionalFilesErr);
+                      }
+                      res.status(200).send({
+                        sample: sample,
+                        actualAdditionalFiles: additionalFiles,
+                      });
+                    },
+                  );
                 }
-            })
-            .then(() => {
-                sendOverseerEmail({type: 'Sample', data: returnedSample}).then((emailResult) => {
-                    res.status(200).send({ sample: returnedSample })
-                })
-            })
-            .catch(err => {
-                console.error('ERROR', err);
-                res.status(500).send({ error: err })
-            })
+              });
+            } catch (e) {
+              console.error(e, e.message);
+              res.status(501).send({ error: "unexpected readdir error" });
+            }
+          } else {
+            res.status(501).send({ error: "not found" });
+          }
+        })
+        .catch((err) => {
+          res.status(500).send({ error: err });
+        });
+    } else {
+      res.status(500).send({ error: new Error("param :id not provided") });
+    }
+  });
 
-        // })
-        // .catch(err => {
-        //     res.status(500).send({ error: err })
-        // })
+router
+  .route("/samples/new")
+  .all(isAuthenticated)
+  .post((req, res) => {
+    const newSample = new Sample({
+      name: req.body.name,
+      project: req.body.project,
+      scientificName: req.body.scientificName,
+      commonName: req.body.commonName,
+      ncbi: req.body.ncbi,
+      conditions: req.body.conditions,
+      owner: req.body.owner,
+      group: req.body.group,
+      // --- NEW: Add tplexCsv field ---
+      tplexCsv: req.body.tplexCsv, // Mongoose will handle `null` or `undefined`
+      // --- END NEW ---
     });
+
+    let returnedSample;
+    newSample
+      .save()
+      .then(async (savedSample) => {
+        returnedSample = savedSample;
+        const additionalFiles = req.body.additionalFiles;
+
+        if (additionalFiles.length) {
+          try {
+            return await sortAdditionalFiles(
+              additionalFiles,
+              "sample",
+              returnedSample._id,
+              returnedSample.path,
+            );
+          } catch (e) {
+            // if issue with files, remove newProject
+            await Sample.deleteOne({ _id: returnedSample._id });
+            return Promise.reject(e);
+          }
+        } else {
+          return Promise.resolve();
+        }
+      })
+      .then(() => {
+        sendOverseerEmail({ type: "Sample", data: returnedSample }).then(
+          (emailResult) => {
+            res.status(200).send({ sample: returnedSample });
+          },
+        );
+      })
+      .catch((err) => {
+        console.error("ERROR", err);
+        res.status(500).send({ error: err });
+      });
+  });
 
 module.exports = router;
